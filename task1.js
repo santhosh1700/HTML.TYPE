9982bde4db24bc2a493382d34b4c945c190c6b2d
santhoshkumar <ssanthosh6700@gmail.com>
2025-08-01 14:12:21 +0530

    Create task1.js

---
+++ task1.js
@@ -0,0 +1,50 @@
+let num = 5;
+
+if (num > 0) {
+  console.log("Positive");
+}
+
+if (num % 2 === 0) {
+  console.log("Even");
+} else {
+  console.log("Odd");
+}
+
+let grade = 85;
+
+if (grade >= 90) {
+  console.log("A");
+} else if (grade >= 80) {
+  console.log("B");
+} else if (grade >= 70) {
+  console.log("C");
+} else {
+  console.log("Fail");
+}
+
+let day = 3;
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("Saturday");
+    break;
+  case 7:
+    console.log("Sunday");
+    break;
+  default:
+    console.log("Invalid day");
+}
